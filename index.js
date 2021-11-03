3bcfe7f4371b3121dbe595d45881fe13233f064b
Kevin <kevaschm713@gmail.com>
2021-11-02 21:38:15 -0500

    updated json again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/json"});
+    response.writeHead(200, {"Content-Type": "application/json"});
     response.end("Hello World! Testing new stuff");
 });
 
